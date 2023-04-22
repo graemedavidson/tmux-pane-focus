0f4fe0bcd4254608901a0341fd1b6a4b8dfab274
Graeme Davidson <graedavidson@gmail.com>
2023-04-22 14:36:09 +0100

    updated tests

--- spec/functions_spec.sh
+++ spec/functions_spec.sh
@@ -20,6 +20,52 @@
 # END
 # )"
 
+
+
+
+
+
+# Tests
+Describe 'check get_active_pane'
+  Include scripts/functions.sh
+
+  # window height
+  # window width
+  # active percentage
+  # mock data (index height width top bottom left right)
+  # result (index resize_height resize_width min_height min_width top bottom left right height width)
+  Parameters
+    100 100 50 "0-50-100-0-49-0-99" "0-false-false-49-49-0-49-0-99-50-100"
+    100 100 60 "0-50-100-0-49-0-99" "0-true-false-59-59-0-49-0-99-50-100"
+    100 100 70 "0-50-100-0-49-0-99" "0-true-false-69-69-0-49-0-99-50-100"
+    100 100 80 "0-50-100-0-49-0-99" "0-true-false-79-79-0-49-0-99-50-100"
+    100 100 90 "0-50-100-0-49-0-99" "0-true-false-89-89-0-49-0-99-50-100"
+
+    100 100 50 "0-25-25-0-49-0-99" "0-true-true-49-49-0-49-0-99-25-25"
+    100 100 60 "0-25-25-0-49-0-99" "0-true-true-59-59-0-49-0-99-25-25"
+    100 100 70 "0-25-25-0-49-0-99" "0-true-true-69-69-0-49-0-99-25-25"
+    100 100 80 "0-25-25-0-49-0-99" "0-true-true-79-79-0-49-0-99-25-25"
+    100 100 90 "0-25-25-0-49-0-99" "0-true-true-89-89-0-49-0-99-25-25"
+
+    1000 1000 50 "0-250-250-0-249-0-249" "0-true-true-499-499-0-249-0-249-250-250"
+  End
+
+  It 'returns correct values for active pane'
+
+    shellspec_mock tmux <<-EOF
+echo "${4}"
+EOF
+
+    When call get_active_pane "${1}" "${2}" "${3}"
+    The output should eq "${5}"
+  End
+End
+
+
+
+
+
+
 # Describe 'check get_split_count'
 #   Include scripts/functions.sh
 
@@ -42,6 +88,20 @@
 #     The output should eq "${3}"
 #   End
 # End
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Describe 'check get_settings'
 #   Include scripts/functions.sh
@@ -69,19 +129,19 @@
 #   End
 # End
 
-# Does not have any outputs to test
-# ToDo: review adding a debug output?
-# Describe 'check resize_pane'
-#   Include scripts/functions.sh
+# # Does not have any outputs to test
+# # ToDo: review adding a debug output?
+# # Describe 'check resize_pane'
+# #   Include scripts/functions.sh
 
-#   Parameters
-#   End
+# #   Parameters
+# #   End
 
-#   It 'correctly resizes pane'
-#     When call resize_pane
-#     The output should eq
-#   End
-# End
+# #   It 'correctly resizes pane'
+# #     When call resize_pane
+# #     The output should eq
+# #   End
+# # End
 
 # Describe 'check check_active_pane'
 #   Include scripts/functions.sh
